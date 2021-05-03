ae2f779afa93349b0bc34f89435240c6c161f891
Carlos Castaneda Largacha <ccastanedal@psl.com.co>
2021-05-03 16:05:14 -0500

    first commit

---
+++ src/main/java/com/carlosc/crud/example/Product.java
@@ -0,0 +1,28 @@
+package com.carlosc.crud.example;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    private Integer id;
+    private String name;
+    private float price;
+
+    public Product() {
+    }
+
+    public Product(Integer id, String name, float price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    public Integer getId() {
+        return id;
+    }
+}
